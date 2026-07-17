22b8f969a28a02a413537d67085dd8d6c74e9676
Fernando Kaspary Fink <85769581+FKF7@users.noreply.github.com>
2026-07-17 16:34:33 +0000

    cooler message on initial page 😎

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home() {
-    return <h1>Parabéns Thiago Vito Pai de Família!</h1>;
+    return (
+        <>
+            <h1>Parabéns Thiago Vito Pai de Família (e Mari também)!</h1>
+            <h2>#TeamPlay5 independente do resultado 😎</h2>
+            <p>PS: Não deixe o número que não deve ser nomeado e outros brainrots aleatórios dominarem</p>
+        </>
+    );
 }
 
 export default Home; 
