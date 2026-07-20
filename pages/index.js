8239cf87d1d941541e6c9a9b070591f0f1da13cf
FKF7 <fernandokfink@gmail.com>
2026-07-20 19:54:44 -0300

    added scripts `lint:check` & `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <div>Rolled back to testing cause I already caused an impact</div>;
+  return <div>Rolled back to testing cause I already caused an impact</div>;
 }
 
-export default Home; 
+export default Home;
