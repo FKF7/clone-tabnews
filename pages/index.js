bab962488e4d115e61decd04128c8c420c9309f7
FKF7 <fernandokfink@gmail.com>
2026-07-20 12:44:48 -0300

    new message for new deploy

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,5 @@
 function Home() {
-    return (
-        <>
-            <h1>Parabéns Thiago Vito Pai de Família (e Mari também)!</h1>
-            <h2>#TeamPlay5 independente do resultado 😎</h2>
-            <p>PS: Não deixe o número que não deve ser nomeado e outros brainrots aleatórios dominarem</p>
-        </>
-    );
+    return <div>Rolled back to testing cause I already caused an impact</div>;
 }
 
 export default Home; 
